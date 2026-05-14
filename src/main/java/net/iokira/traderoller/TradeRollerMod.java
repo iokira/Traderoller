d747fff82e2a4b29cbe42dd6ad7eca933d9f9dbf
iokira <iam.iokiller@gmail.com>
2026-05-14 23:07:50 +0900

    rename trade reroller -> traderoller

---
+++ src/main/java/net/iokira/traderoller/TradeRollerMod.java
@@ -0,0 +1,8 @@
+package net.iokira.traderoller;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(TradeRollerMod.MOD_ID)
+public class TradeRollerMod {
+    public static final String MOD_ID = "traderoller";
+}
